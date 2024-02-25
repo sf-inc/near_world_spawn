efdc7f1e565ff6db3a8c5451922a35fba8fdec09
charlyb01 <charly5bollinger@gmail.com>
2024-02-25 16:01:51 -0500

    Initial setup

---
+++ src/main/java/com/github/charlyb01/near_world_spawn/NearWorldSpawn.java
@@ -0,0 +1,13 @@
+package com.github.charlyb01.near_world_spawn;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NearWorldSpawn implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
